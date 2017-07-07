0659bb8c2b87ec12f848b8df0605ac8f119f0211
fyzzy1943 <fyzzy1943@gmail.com>
2017-07-07 14:25:28 +0800

    #

---
+++ database/migrations/2017_07_07_031717_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('cover');
+            $table->text('abstract');
+            $table->text('doc_md');
+            $table->text('doc_html');
+            $table->unsignedTinyInteger('status')->comment('状态：0草稿1发布2隐藏');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
