b92735e5639aad9c78d927a925014b52b59855bd
fyzzy1943 <fyzzy1943@gmail.com>
2017-07-23 11:59:35 +0800

    #

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -4,44 +4,28 @@
   {{--<link rel="stylesheet" href="/editor/style.css" />--}}
   {{--<link rel="stylesheet" href="/editor/editormd.css" />--}}
   <link rel="stylesheet" href="/editor/css/editormd.preview.min.css" />
-    <style>
-        /*#sidebar {*/
-            /*width: 400px;*/
-            /*height: 100%;*/
-            /*position: fixed;*/
-            /*top: 0;*/
-            /*right: 0;*/
-            /*overflow: hidden;*/
-            /*background: #fff;*/
-            /*z-index: 100;*/
-            /*padding: 18px;*/
-            /*border: 1px solid #ddd;*/
-            /*border-top: none;*/
-            /*border-bottom: none;*/
-        /*}*/
-
-        /*#sidebar:hover {*/
-            /*overflow: auto;*/
-        /*}*/
-    </style>
+  <link rel="stylesheet" href="{{ asset('fantasy/detail.css') }}">
 @endsection
 
 @section('content')
-    <div class="container">
-        <h1>{{ $article->title }}</h1>
-        <div id="sidebar">
-            {{--<h1>Table of Contents</h1>--}}
-            <div class="markdown-body editormd-preview-container" id="custom-toc-container">#custom-toc-container</div>
-        </div>
-        <article>
-            <div id="article">
-                <textarea title="article" style="display: none">{!! $article->doc_md !!}</textarea>
-            </div>
-        </article>
-        <footer>
-            <a href="/article" class="btn btn-default">返回</a>
-        </footer>
+  <div class="loading">
+    <img src="{{ asset('shadow/loading.gif') }}">
+  </div>
+  <div class="container" style="display: none">
+    <h1>{{ $article->title }}</h1>
+    <div id="sidebar">
+      {{--<h1>Table of Contents</h1>--}}
+      <div class="markdown-body editormd-preview-container" id="custom-toc-container">#custom-toc-container</div>
     </div>
+    <article>
+      <div id="article">
+        <textarea title="article" style="display: none">{!! $article->doc_md !!}</textarea>
+      </div>
+    </article>
+    <footer>
+      <a href="/article" class="btn btn-default">返回</a>
+    </footer>
+  </div>
 @endsection
 
 @section('script')
@@ -53,11 +37,15 @@
   <script src="/editor/lib/sequence-diagram.min.js"></script>
   <script src="/editor/lib/flowchart.min.js"></script>
   <script src="/editor/lib/jquery.flowchart.min.js"></script>
+  <script>
 
+  </script>
 
-  <script src="/editor/editormd.js"></script>
+  <script src="/editor/editormd.min.js"></script>
   <script>
     $(function () {
+        $('.loading').fadeOut();
+        $('.container').fadeIn();
         var article;
 
         article = editormd.markdownToHTML("article", {
--- resources/views/layouts/avalon.blade.php
+++ resources/views/layouts/avalon.blade.php
@@ -43,6 +43,8 @@
 
 <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 @yield('script')
+
 </body>
 </html>
