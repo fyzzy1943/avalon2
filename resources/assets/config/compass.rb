64d2afd783c95d5a1c322932963ccfbe156bc774
fyzzy1943 <fyzzy1943@gmail.com>
2017-07-25 13:55:12 +0800

    #

--- resources/assets/config/compass.rb
+++ resources/assets/config/compass.rb
@@ -1,6 +1,10 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
+# The environment mode. Defaults to :development, can also be :production
+environment = :development
+# environment = :production
+
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "../../public/fantasy"
@@ -10,13 +14,15 @@ javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+output_style = (environment == :production) ? :compressed : :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
+
+# sourcemap = true
 
 
 # If you prefer the indented syntax, you might want to regenerate this
