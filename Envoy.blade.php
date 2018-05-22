0d2341de65638c4b2c08b42afc57033c65640781
fyzzy1943 <fyzzy1943@gmail.com>
2018-05-22 14:58:52 +0800

    #wip.ddwq

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -14,12 +14,13 @@
     cd app/avalon2
     php artisan config:cache
     php artisan route:cache
-    php artisan view:cache
+    php artisan view:clear
 @endtask
 
 @task('composer')
     cd app/avalon2
     composer install
+    composer dumpautoload
 @endtask
 
 @story('deploy', ['on' => 'web'])
